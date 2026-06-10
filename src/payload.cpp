df5d9578a2dad22c3c0c56c1812d1e6b72484837
hyperion0801 <quadcore1472@gmail.com>
2026-06-10 18:58:10 +0200

    feat: add some miku, this payload needs more of it.

--- src/payload.cpp
+++ src/payload.cpp
@@ -4,6 +4,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+// Thank you, Stack Overflow!
+unsigned short repeatLoop = 10;
+
 void displayText(std::string msg)
 {
   for (const auto e : std::filesystem::directory_iterator("/dev")) {
@@ -34,18 +37,46 @@ void stillUsingThisPC()
   displayText("LoL still using this PC?");
 }
 
-void CPsihTgnisUllits() {
-  displayText("?CP siht gnisu llits LoL");
+void CPsihTgnisUllits()
+{
+  displayText("\n?CP siht gnisu llits LoL");
 }
+
+/* This will be added in later, right now it's broken.'
+void loremIpsum()
+{
+  char ipsumText[]= "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+
+  for(int i = 0; i < (sizeof(ipsumText) / sizeof(ipsumText[0])) {
+
+  }
+
+}
+*/
 
 int main()
 {
   sleep(3);
   stillUsingThisPC();
-  sleep(20);
-  displayText("System Unstable!!");
-  sleep(15);
-  displayText("Oh no! Kernel Panic incoming!!!");
+  sleep(2);
+  displayText("\nSystem Unstable!!");
   sleep(1);
-  displayText("( i am lying :) )");
+  displayText("\nOh no! Kernel Panic incoming!!!");
+  sleep(1);
+  displayText("\n( i am lying :) )");
+  sleep(2);
+  CPsihTgnisUllits();
+  sleep(5);
+  displayText("\nFun fact about AI: it sucks ass. So I'll fire up ChatGPT now to make you suffer.");
+  while(repeatLoop--) {
+    system("xdg-open https://chatgpt.com");
+  }
+  sleep(5);
+  displayText("Is this too little AI for you?");
+  sleep(5);
+  displayText("\nYo did you hear about the-\nHEADPHONE WARNING IN 5 SECONDS");
+  system("mpv --no-video https://www.youtube.com/watch?v=vPlFkyXY6L0 > /dev/null");
+  displayText("\nYou're a twerp for not liking Miku!\nWhat about the Kagamine's?");
+  system("mpv --no-video https://www.youtube.com/watch?v=oEkGC2HV7rc > /dev/null");
+  displayText("\nOkay how can I please you now?\n");
 }
