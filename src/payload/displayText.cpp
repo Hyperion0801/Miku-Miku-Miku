67566fa584a3f7ced165982d4abdf4ddfe9de9f3
hyperion0801 <quadcore1472@gmail.com>
2026-07-24 19:38:43 +0200

    fix: placeholder

--- src/payload/displayText.cpp
+++ src/payload/displayText.cpp
@@ -9,8 +9,7 @@
 void displayText(std::string msg)
 {
   // for gets all things in /dev
-  for (const auto e : std::filesystem::directory_iterator("/dev"))
-  {
+  for (const auto e : std::filesystem::directory_iterator("/dev")) {
     std::string name = e.path();
     // checks if not tty then continue
     if (!(!name.rfind("tty", 0))) {
@@ -41,8 +40,7 @@ void loremIpsum()
   const char ipsumText[IPSUMSIZE] = "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.";
 
   // Sooooooooo, sorry flumpsi but I hardcoded this because it's simple.
-  for(int i = 0; i < IPSUMSIZE; i++)
-  {
+  for(int i = 0; i < IPSUMSIZE; i++) {
     std::cout << ipsumText[i] << std::flush;
     usleep(33333);
   }
